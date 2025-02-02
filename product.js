400c889e8227c1e90e6ca79b5ae6e5b684552b21
imfazal0 <boyswild104@gmail.com>
2025-02-02 22:22:37 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,122 @@
+const productinfo=[
+    {
+    id: 1,
+    image: "pro-img/redmi note 14.png",
+    name: "Redmi Note 14 256/8 new Model ruby diomond snapdragon",
+    rating: {
+        star: "images/rating/45.jpg",
+        ratingcount: 45
+    },
+    price: 256
+},
+{
+    id: 2,
+    image: "pro-img/redmi note 14 256.png",
+    name: "Redmi Note 14 256/8 Model snapdragon",
+    rating: {
+        star: "images/rating/40.jpg",
+        ratingcount: 40
+    },
+    price: 200
+},
+{
+    id: 3,
+    image: "pro-img/xiaomi 11 T.jpg",
+    name: "Xiaomi 11T 128 /6 Model snapdragon",
+    rating: {
+        star: "images/rating/35.jpg",
+        ratingcount: 35
+    },
+    price: 125
+},
+{
+    id: 4,
+    image: "pro-img/xiaomi 11t (2).jpg",
+    name: "Xiaomi 11T 128 /6 Model snapdragon",
+    rating: {
+        star: "images/rating/35.jpg",
+        ratingcount: 35
+    },
+    price: 150
+},
+{
+    id: 5,
+    image: "pro-img/xiaomi 11t (2).jpg",
+    name: "Xiaomi 11T 128 /6 Model snapdragon",
+    rating: {
+        star: "images/rating/35.jpg",
+        ratingcount: 35
+    },
+    price: 140
+},
+{
+    id: 6,
+    image: "pro-img/xiaomi 14 ultra.jpg",
+    name: "Xiaomi 11T 128 /6 Model snapdragon",
+    rating: {
+        star: "images/rating/35.jpg",
+        ratingcount: 35
+    },
+    price: 125
+},
+{
+    id: 7,
+    image: "pro-img/xiaomi new phone.jpg",
+    name: "Xiaomi 11T 128 /6 Model snapdragon",
+    rating: {
+        star: "images/rating/35.jpg",
+        ratingcount: 35
+    },
+    price: 200
+},
+{
+    id: 8,
+    image: "pro-img/xiaomi new version.jpg",
+    name: "Xiaomi 11T 128 /6 Model snapdragon",
+    rating: {
+        star: "images/rating/35.jpg",
+        ratingcount: 35
+    },
+    price: 250
+},
+{
+    id: 9,
+    image: "pro-img/xiaomi 14.png",
+    name: "Xiaomi 11T 128 /6 Model snapdragon",
+    rating: {
+        star: "images/rating/35.jpg",
+        ratingcount: 35
+    },
+    price: 500
+},
+{
+    id: 10,
+    image: "pro-img/xiaomi new version.jpg",
+    name: "Xiaomi 11T 128 /6 Model snapdragon",
+    rating: {
+        star: "images/rating/35.jpg",
+        ratingcount: 35
+    },
+    price: 560
+},
+{
+    id: 11,
+    image: "pro-img/xiaomi 14.png",
+    name: "Xiaomi 11T 128 /6 Model snapdragon",
+    rating: {
+        star: "images/rating/35.jpg",
+        ratingcount: 35
+    },
+    price: 120
+},
+{
+    id: 12,
+    image: "pro-img/xiaomi mobile phone.jpg",
+    name: "Xiaomi 11T 128 /6 Model snapdragon",
+    rating: {
+        star: "images/rating/35.jpg",
+        ratingcount: 35
+    },
+    price: 440
+}
+]
